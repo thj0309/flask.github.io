2dd87864eb5eaa8255b589ae3607ef11e580302a
Toto Hyojun Tak <thj0309@gmail.com>
2021-10-19 22:59:40 +0900

    Add files via upload

--- pybo/views/question_views.py
+++ pybo/views/question_views.py
@@ -72,7 +72,7 @@ def create():
                             create_date=datetime.now(), user=g.user)
         db.session.add(question)
         db.session.commit()
-        return redirect(url_for('main.index'))
+        return redirect(url_for('question._list'))
     return render_template('question/question_form.html', form=form)
 
 
