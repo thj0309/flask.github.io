50e8dc30221503c5c43656c029eb1b3dc029b0ac
TotoHyojunTak <thj0309@msn.com>
2021-10-31 15:40:17 +0900

    test

--- mac_batch_prd.sh
+++ mac_batch_prd.sh
@@ -1,4 +1,6 @@
 
 export FLASK_APP=pybo/__init__.py
 export FLASK_ENV=production
-export APP_CONFIG_FILE=..\config\production.py
+export APP_CONFIG_FILE=../config/production.py
+export set FLASK_HOST=0.0.0.0
+
